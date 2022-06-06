323414f811647df5bbdda6be8b4b6336a712ffb3
camille-granier <granierc@protonmail.com>
2022-06-06 17:46:37 +1000

    delete user controller added

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -22,6 +22,39 @@ module.exports.userInfo = async (req, res) => {
 
 //Update one user info
 module.exports.updateUser = async (req, res) => {
-    if (!ObjectID.isValid(req.params))
+    if (!ObjectID.isValid(req.params.id))
         return res.status(400).send('ID unkown ' + req.params.id)
+    
+    try {
+        await UserModel.findOneAndUpdate(
+            { _id: req.params.id },
+            {
+                $set: {
+                    bio: req.body.bio,
+                }
+            },
+            { new: true, upsert: true, setDefaultsOnInsert: true},
+            (err, docs) => {
+                if(!err) return res.send(docs);
+                if (err) return res.status(500).send({ message: err})
+            }
+        )
+    }  catch (err) {
+        return res.status(500).json({ message: err })
+    }
+}
+
+//delete user
+module.exports.deleteUser = async (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+        return res.status(400).send('ID unkown ' + req.params.id)
+
+        try {
+            await UserModel.deleteOne(
+                { _id: req.params.id }).exec();
+                res.status(200).json({ message: "Successfully deleted"})
+            
+        } catch(err) {
+            return res.status(500).json({ message: err })
+        }
 }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -11,6 +11,8 @@ router.get('/', userController.getAllUsers)
 //one user
 router.get('/:id', userController.userInfo)
 //update
-router.put('./:id', userController.updateUser)
+router.put('/:id', userController.updateUser)
+//delete
+router.delete('/:id', userController.deleteUser)
 
 module.exports = router;
