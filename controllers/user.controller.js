5e62b36bb49f77af78716200d3bbfa608084d4b6
camille-granier <granierc@protonmail.com>
2022-06-06 17:05:46 +1000

    setting up update user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,9 +1,27 @@
 const UserModel = require('../models/user.model');
 const ObjectID = require('mongoose').Types.ObjectId;
 
-
+//get all users
 module.exports.getAllUsers = async (req, res) => {
     //avoid to select password
     const users = await UserModel.find().select('-password');
     res.status(200).json(users);
 };
+
+//get one user
+module.exports.userInfo = async (req, res) => {
+    console.log(req.params);
+    if (!ObjectID.isValid(req.params))
+    return res.status(400).send('ID unknown ' + req.params.id)
+
+    UserModel.findById(req.params.id, (err, docs) => {
+        if(!err) res.send(docs);
+        else console.log('ID unknown ' + err);
+    }).select('-password');
+}
+
+//Update one user info
+module.exports.updateUser = async (req, res) => {
+    if (!ObjectID.isValid(req.params))
+        return res.status(400).send('ID unkown ' + req.params.id)
+}
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,6 +6,11 @@ const userController = require('../controllers/user.controller');
 router.post("/register", authController.signUp);
 
 //user DB
+//all users
 router.get('/', userController.getAllUsers)
+//one user
+router.get('/:id', userController.userInfo)
+//update
+router.put('./:id', userController.updateUser)
 
 module.exports = router;
